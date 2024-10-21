2e3bae1a59fdc06437726fc705c5501d6dd9b611
carlobex <ciarnellicarloedoardo@gmail.com>
2024-10-21 12:47:03 +0200

    add header

--- frontend-web/src/components/Header.tsx
+++ frontend-web/src/components/Header.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from '../styles/Header.module.scss';
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      {/* Content will be added later */}
+      <div className={styles.container}>
+        <div className={styles.userInfo}>
+          <div className={styles.avatarContainer}>
+            <Image
+              src="/User.svg"
+              alt="User Avatar"
+              width={24}
+              height={24}
+              className={styles.avatarIcon}
+            />
+          </div>
+          <div className={styles.welcomeText}>
+            <p>Welcome</p>
+            <p>User</p>
+          </div>
+        </div>
+          <Image
+            src="/Menu.svg"
+            alt="Menu"
+            width={24}
+            height={24}
+            className={styles.menuIcon}
+          />
+      </div>
     </header>
   );
 };
