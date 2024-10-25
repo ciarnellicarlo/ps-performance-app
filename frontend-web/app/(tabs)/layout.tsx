05dbc5cd242799ee0fc4a6462ae71593cda20aee
carlobex <ciarnellicarloedoardo@gmail.com>
2024-10-25 14:42:20 +0200

    fix font and aspect ratio

--- frontend-web/app/(tabs)/layout.tsx
+++ frontend-web/app/(tabs)/layout.tsx
@@ -1,11 +1,21 @@
-import { Poppins } from 'next/font/google'
 import '@/styles/globals.scss'
+import { Poppins } from 'next/font/google'
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['100', '300', '500'],
   variable: '--font-poppins',
 })
+
+export const metadata = {
+  viewport: {
+    width: 'device-width',
+    initialScale: 1.0,
+    viewportFit: 'cover',
+  },
+  title: 'PlayStation Performance',  // Your app title
+  description: 'Explore game benchmarks by resolution and FPS for every PlayStation version.',
+}
 
 export default function RootLayout({
   children,
